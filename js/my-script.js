4da35ca938bc872f9c27e6f904c88732de3e325f
gurdeepsingh2000 <gurdeepsingh20000329@gmail.com>
2021-07-01 12:25:26 +0200

    update

--- js/my-script.js
+++ js/my-script.js
@@ -99,46 +99,38 @@ new Vue (
                             }
                         ],
                     },
-                    {
-                        name: 'Luisa',
-                        avatar: '_4',
-                        visible: true,
-                        messages: [
-                            {
-                                date: '10/01/2020 15:30:55',
-                                text: 'Lo sai che ha aperto una nuova pizzeria?',
-                                status: 'sent'
-                            },
-                            {
-                                date: '10/01/2020 15:50:00',
-                                text: 'Si, ma preferirei andare al cinema',
-                                status: 'received'
-                            }
-                        ],
-                    },{
-                        name: 'Luisa',
-                        avatar: '_4',
-                        visible: true,
-                        messages: [
-                            {
-                                date: '10/01/2020 15:30:55',
-                                text: 'Lo sai che ha aperto una nuova pizzeria?',
-                                status: 'sent'
-                            },
-                            {
-                                date: '10/01/2020 15:50:00',
-                                text: 'Si, ma preferirei andare al cinema',
-                                status: 'received'
-                            }
-                        ],
-                    },
             ],
+            actualindex: 0
             
         },
         methods:{
             user_avatar: function(index){
                 return this.user_img[index]
-            } 
+            },
+            sender: function(sent){
+                if(sent === 'sent'){
+                    return true
+                }else{
+                    return
+                }
+            },
+            receiver: function(receive){
+                if(receive ==='received'){
+                    return true
+                }
+            }
         }
     }
 )
+
+
+
+/*
+Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+Click sul contatto mostra la conversazione del contatto cliccato
+Milestone 3
+Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+Milestone 4
+Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+*/
