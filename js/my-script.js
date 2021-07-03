fb9b32ddc5183f8a01a8b6e3d0097b0feaccc9d7
gurdeepsingh2000 <gurdeepsingh20000329@gmail.com>
2021-07-03 22:14:50 +0200

    sistemato disposizione messaggi

--- js/my-script.js
+++ js/my-script.js
@@ -142,18 +142,9 @@ new Vue (
                 const getTimeAndDate = dayjs()
                 return getTimeAndDate.format("DD/MM/YYYY HH:mm:ss")
               }
-        }
+        },
     }
 )
 
 
 
-/*
-Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
-Click sul contatto mostra la conversazione del contatto cliccato
-Milestone 3
-Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
-Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
-Milestone 4
-Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
-*/
